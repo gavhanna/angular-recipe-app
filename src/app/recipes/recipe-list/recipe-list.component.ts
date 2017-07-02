a8f4359c223df8fb35a1c4a316d72b15c3faf216
Gav Hanna <gavhanna@gmail.com>
2017-07-02 16:34:38 +0100

    Section 3 --  9/16

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from 'app/recipes/recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+   recipes: Recipe[] = [
+     new Recipe('A Test Recipe', 'Just a test', 'https://cdn.pixabay.com/photo/2015/04/08/13/14/food-712666_960_720.jpg')
+   ];
 
   constructor() { }
 
