6ad6b6d4cf3c8dc0e480c6b9eb310965b0632b1d
Gav Hanna <gavhanna@gmail.com>
2017-08-13 18:29:03 +0100

    minor bugfix

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -19,7 +19,7 @@ export class RecipeListComponent implements OnInit, OnDestroy {
     private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.recipeService.recipesChanged
+    this.subscription = this.recipeService.recipesChanged
       .subscribe(
         (recipes: Recipe[]) => {
           this.recipes = recipes;
