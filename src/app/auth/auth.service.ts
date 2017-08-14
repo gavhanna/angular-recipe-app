54c12303027f885278452b6768f98be5b70c327f
Gav Hanna <gavhanna@gmail.com>
2017-08-14 22:19:57 +0100

    Section 20 -- 11/14

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -32,4 +32,13 @@ export class AuthService {
       );
     return this.token;
   }
+
+  isAuthenticated() {
+    return this.token != null;
+  }
+
+  logout() {
+    firebase.auth().signOut();
+    this.token = null;
+  }
 }
