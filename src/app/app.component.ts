ca6f6d21a27fd798ec5c0d4d54fbf1ee6a7d3f54
Gav Hanna <gavhanna@gmail.com>
2017-08-15 08:21:14 +0100

    user can stay logged in if authKey is in local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import * as firebase from 'firebase';
+import { AuthService } from 'app/auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -10,14 +11,22 @@ import * as firebase from 'firebase';
 export class AppComponent implements OnInit {
   loadedFeature = 'recipe';
 
+  constructor(private authService: AuthService) {}
+
   onNavigate(feature: string) {
     this.loadedFeature = feature;
   }
 
   ngOnInit() {
+    const authUser = Object.keys(window.localStorage).filter(item => {
+      return item.startsWith('firebase:authUser')
+    });
+
     firebase.initializeApp({
       apiKey: 'AIzaSyDWjowDrw_BtYxCmKWnu5av92V1KYBplxg',
       authDomain: 'ng-recipe-f8761.firebaseapp.com',
     });
+
+    this.authService.token = authUser[0];
   }
 }
